d66d9100d5e14440881e93cf572f90ab206f5fa8
1 <ypopov2005@gmail.com>
2025-02-08 12:42:44 +0300

    2 lesson complete

--- base/2_lesson.cpp
+++ base/2_lesson.cpp
@@ -7,10 +7,34 @@ void test_1()
     std::cout << std::numeric_limits<T>::min() << " " << std::numeric_limits<T>::max() << std::endl;
 }
 
+auto test_2()
+{
+    int a = 6, b = 4;
+    return static_cast<double>(a) / b;
+}
+
+void task_1(void)
+{
+    double sm;
+    std::cin >> sm;
+    double res = sm / 2.54;
+    std::cout << res << std::endl;
+}
+
+void task_2(void)
+{
+    long long n;
+    std::cin >> n;
+    __int128_t result = (static_cast<__int128_t>(n) * (static_cast<__int128_t>(n) + 1)) / 2;
+    std::cout << static_cast<long long>(result) << "\n";
+}
+
 auto main() -> int
 {
     int rc = 0;
-    int value;
-    test_1<decltype(value)>();
+    // test_1<decltype(value)>();
+    // std::cout << test_2() << std::endl;
+    // task_1();
+    task_2();
     return rc;
 }
